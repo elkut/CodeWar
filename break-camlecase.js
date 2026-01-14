414ae304a8a20c7309508cd7f8f999368d42fadb
Alkut Askar <elkut.esker@gmail.com>
2026-01-13 22:00:13 -0500

    added daily codewar

---
+++ break-camlecase.js
@@ -0,0 +1,22 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+//     Example
+// "camelCasing"  => "camel Casing"
+// "identifier"   => "identifier"
+// ""             => ""
+
+function solution(string) {
+    let arr = string.split('');
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i].toUpperCase()) {
+            arr[i] = ' ' + arr[i];
+        }
+    }
+
+    return arr.join('');
+}
+
+function solution(string) {
+    return (string.replace(/([A-Z])/g, ' $1'));
+}
